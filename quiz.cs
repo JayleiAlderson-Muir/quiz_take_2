87ef11009266702e351960a7fb1dfd84d9113ca3
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2022-09-20 11:22:57 +1200

    fixed global score

--- quiz.cs
+++ quiz.cs
@@ -8,9 +8,9 @@ namespace quizffs
         public static int score = 0;
         public static void Main()
         {
-            question1.Question1(score); //calling question 1 class
-            question2.q2(score); //calling question 2 class
-            question3.Question3(score); //calling question 3 class
+            score = question1.Question1(score); //calling question 1 class
+            score = question2.q2(score); //calling question 2 class
+            score = question3.Question3(score); //calling question 3 class
             Console.WriteLine($"Your final score is {score}");
             Console.ReadLine();
         }
